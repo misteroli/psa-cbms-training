eabd611b0656dc9790dba542ed6e1235876eaf52
Oli Tori <bourgeiosue@gmail.com>
2025-12-02 12:28:32 +0800

    Refactor starwars data analysis and visualization
    Updated data manipulation and visualization steps for the starwars dataset, including filtering, summarizing, and mutating operations.

--- Fajardo - Packages and Tidyverse.R
+++ Fajardo - Packages and Tidyverse.R
@@ -1,8 +1,5 @@
-library(dplyr) 
-
-View(starwars)
 #1
-select(starwars, ends_with("color")) 
+select(starwars, name, ends_with("color"))
 
 
 #2
@@ -18,30 +15,51 @@ summarize(
   average_height = mean(height  * 0.39370079, na.rm = TRUE)
 )
 
-
 summarize(
   group_by(starwars, homeworld), 
   average_height = mean(height, na.rm = TRUE)
 )
 
+
+
+
 #4
+
+View(
 arrange(
-summarize(
-  group_by(starwars, species, gender),
-  count = n()
-), desc(count)
+  summarize(
+    group_by(filter(starwars, !is.na(species)), species, gender),
+    count = n()
+  ), desc(count)
+)
 )
 
+#or
+
+View(
+  arrange(
+    summarize(
+      group_by(starwars, species, gender),
+      count = n()
+    ), desc(count)
+  )
+)
+
+
+
+
 #5
-starwars <- starwars %>%
-  mutate(sex = case_when(
+mutate(starwars,
+  sex = case_when(
     is.na(sex) ~ 8,
     sex == "male" ~ 1,
     sex == "female" ~ 2,
     sex == "hermaphroditic" ~ 3,
-    sex == "none" ~ 4,
-    TRUE ~ NA_real_
-  ))
+    sex == "none" ~ 4
+  )
+)
 
 
-print(starwars)
+View(starwars)
+
+help(is.na)
